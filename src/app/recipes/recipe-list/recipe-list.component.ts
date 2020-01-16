3c18c325498f690b028fdda024c72cabcc15dae5
Mykhailo Kazakov <kamiournew@gmail.com>
2020-01-17 00:48:44 +0200

    completed basic structure of the app

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,11 +1,19 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Recipe } from '../recipe.model';
+
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('A Test Recipe', 'Test Description', 'https://cdn.pixabay.com/photo/2018/10/31/12/37/healthy-food-3785722_960_720.jpg'),
+    new Recipe('Lasagna', 'A cake with cheese', 'https://live.staticflickr.com/2575/3716908330_0a5a489309_b.jpg'),
+    new Recipe('Chocolate cake', 'A cake with chocolate', 'https://live.staticflickr.com/3735/11370696464_a355682578_b.jpg'),
+    
+  ];
 
   constructor() { }
 
