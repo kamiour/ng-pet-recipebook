326c02abea3c7782138226918ca89d5bd6c5b176
Mykhailo Kazakov <kamiournew@gmail.com>
2020-01-22 01:55:01 +0200

    added shopping-list service and recipe service to the app

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit, EventEmitter, Output } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 import { Recipe } from '../recipe.model';
+import { RecipeService } from '../recipe.service';
 
 @Component({
   selector: 'app-recipe-list',
@@ -8,21 +9,12 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-  @Output() recipeWasSelected = new EventEmitter<Recipe>();
-  recipes: Recipe[] = [
-    new Recipe('A Test Recipe', 'Test Description', 'https://cdn.pixabay.com/photo/2018/10/31/12/37/healthy-food-3785722_960_720.jpg'),
-    new Recipe('Lasagna', 'A cake with cheese', 'https://live.staticflickr.com/2575/3716908330_0a5a489309_b.jpg'),
-    new Recipe('Chocolate cake', 'A cake with chocolate', 'https://live.staticflickr.com/3735/11370696464_a355682578_b.jpg'),
-    
-  ];
+  recipes: Recipe[];
 
-  constructor() { }
+  constructor(private recipeService: RecipeService) { }
 
   ngOnInit() {
-  }
-
-  onRecipeSelected(recipe: Recipe) {
-    this.recipeWasSelected.emit(recipe);
+    this.recipes = this.recipeService.getRecipes();
   }
 
 }
---
+++ src/app/recipes/recipe.service.ts
@@ -0,0 +1,47 @@
+import { Recipe } from './recipe.model';
+import { EventEmitter, Injectable } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
+import { ShoppingListService } from '../shopping-list/shopping-list.service';
+
+@Injectable()
+export class RecipeService {
+    recipeSelected = new EventEmitter<Recipe>();
+
+    private recipes: Recipe[] = [
+        new Recipe(
+            'Porridge',
+            'A breakfast meal',
+            'https://cdn.pixabay.com/photo/2018/10/31/12/37/healthy-food-3785722_960_720.jpg',
+            [
+                new Ingredient('Cucumbers', 2),
+                new Ingredient('Eggs', 3),
+                new Ingredient('Buckwheat', 1)
+            ]),
+        new Recipe(
+            'Lasagna',
+            'A cake with cheese',
+            'https://live.staticflickr.com/2575/3716908330_0a5a489309_b.jpg',
+            [
+                new Ingredient('Cheese', 1),
+                new Ingredient('Eggs', 3),
+            ]),
+        new Recipe(
+            'Chocolate cake',
+            'A cake with chocolate',
+            'https://live.staticflickr.com/3735/11370696464_a355682578_b.jpg',
+            [
+                new Ingredient('Chocolate', 1),
+                new Ingredient('Butter', 1),
+            ]),
+    ];
+
+    constructor(private slService: ShoppingListService) {}
+
+    getRecipes() {
+        return this.recipes.slice();
+    }
+
+    addIngredientsToShoppingList(ingredients: Ingredient[]) {
+        this.slService.addIngredients(ingredients);
+    }
+}
