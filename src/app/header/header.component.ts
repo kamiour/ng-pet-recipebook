aee4174fe59fdd25e18010af9ffb93d0090738d8
Mykhailo Kazakov <mykhailo.kazakov@requestum.com>
2020-01-31 00:30:55 +0200

    completed authentication section

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -21,6 +21,10 @@ export class HeaderComponent implements OnInit, OnDestroy {
         this.dataStorageService.fetchRecipes().subscribe();
     }
 
+    onLogout() {
+        this.authService.logout();
+    }
+
     ngOnInit() {
         this.userSub = this.authService.user.subscribe(user => {
             this.isAuthenticated = !!user;
