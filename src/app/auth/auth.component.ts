d3c11759752bb791917956bb09567d6466c9e94c
Mykhailo Kazakov <mykhailo.kazakov@requestum.com>
2020-02-02 18:45:09 +0200

    shifted auth section to NgRx

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,24 +1,39 @@
-import { Component, ComponentFactoryResolver, ViewChild, OnDestroy } from '@angular/core';
+import { Component, ComponentFactoryResolver, ViewChild, OnDestroy, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
-import { AuthService, AuthResponseData } from './auth.service';
-import { Observable, Subscription } from 'rxjs';
-import { Router } from '@angular/router';
+import { Subscription } from 'rxjs';
 import { AlertComponent } from '../shared/alert/alert.component';
 import { PlaceholderDirective } from '../shared/placeholder/placeholder.directive';
+import * as fromApp from '../store/app.reducer';
+import * as AuthActions from './store/auth.actions';
+import { Store } from '@ngrx/store';
 
 @Component({
     selector: 'app-auth',
     templateUrl: './auth.component.html'
 })
-export class AuthComponent implements OnDestroy {
+export class AuthComponent implements OnDestroy, OnInit {
     @ViewChild(PlaceholderDirective, {static: false}) alertHost: PlaceholderDirective;
+    private storeSub: Subscription;
 
     private closeSub: Subscription;
 
-    constructor(private authservice: AuthService, private router: Router, private componentFactoryResolver: ComponentFactoryResolver) {}
+    constructor(
+        private componentFactoryResolver: ComponentFactoryResolver,
+        private store: Store<fromApp.AppState>
+        ) {}
     isLoginMode = true;
     isLoading = false;
     error: string = null;
+
+    ngOnInit() {
+        this.storeSub = this.store.select('auth').subscribe(authState => {
+            this.isLoading = authState.loading;
+            this.error = authState.authError;
+            if (this.error) {
+                this.showErrorAlert(this.error);
+            }
+        })
+    }
 
     onSwitchMode() {
         this.isLoginMode = !this.isLoginMode;
@@ -31,31 +46,40 @@ export class AuthComponent implements OnDestroy {
         const email = form.value.email;
         const password = form.value.password;
 
-        let authObs: Observable<AuthResponseData>
+        // let authObs: Observable<AuthResponseData>
 
-        this.isLoading = true;
+        // this.isLoading = true;
+
         if (this.isLoginMode) {
-            authObs = this.authservice.login(email, password);
+            // authObs = this.authservice.login(email, password);
+            this.store.dispatch(new AuthActions.LoginStart({
+                email: email,
+                password: password
+            }))
         } else {
-            authObs = this.authservice.signup(email, password);
+            // authObs = this.authservice.signup(email, password);
+            this.store.dispatch(new AuthActions.SignupStart({
+                email: email,
+                password: password
+            }))
         }
 
-        authObs.subscribe((resData) => {
-            console.log(resData);
-            this.isLoading = false;
-            this.router.navigate(['/recipes']);
-        }, (errorMessage) => {
-            console.log(errorMessage);
-            this.error = errorMessage;
-            this.showErrorAlert(errorMessage);
-            this.isLoading = false;
-        });
+        // authObs.subscribe((resData) => {
+        //     console.log(resData);
+        //     this.isLoading = false;
+        //     this.router.navigate(['/recipes']);
+        // }, (errorMessage) => {
+        //     console.log(errorMessage);
+        //     this.error = errorMessage;
+        //     this.showErrorAlert(errorMessage);
+        //     this.isLoading = false;
+        // });
 
         form.reset();
     }
 
     onHandleError() {
-        this.error = null;
+        this.store.dispatch(new AuthActions.ClearError());
     }
 
     private showErrorAlert(message: string) {
@@ -76,6 +100,9 @@ export class AuthComponent implements OnDestroy {
     ngOnDestroy() {
         if (this.closeSub) {
             this.closeSub.unsubscribe();
+        }
+        if (this.storeSub) {
+            this.storeSub.unsubscribe();
         }
     }
 }
